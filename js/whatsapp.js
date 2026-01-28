c2776a3a48b42da3d15b280d5ab137a6a4197e88
anthati2006 <Anthati2006@gmail.com>
2026-01-28 09:43:17 +0530

    Add files via upload

---
+++ js/whatsapp.js
@@ -0,0 +1,45 @@
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxhlOcys92vrrg_uLdJllMDLuKvvYIFZCv3HhMnkrGlo_Z1fhnTdEOJU0VTOG4bX_eDrA/exec";
+
+function submitOrder(e, product) {
+  e.preventDefault();
+
+  const form = e.target;
+  const name = form[0].value;
+  const mobile = form[1].value;
+  const address = form[2].value;
+
+  const data = {
+    type: "order",
+    product: product,
+    name: name,
+    mobile: mobile,
+    address: address
+  };
+
+  fetch(SCRIPT_URL, {
+    method: "POST",
+    body: JSON.stringify(data),
+    mode: "no-cors"
+  });
+
+  const msg = `New Order 🔔
+
+Product: ${product}
+Name: ${name}
+Mobile: ${mobile}
+Address: ${address}
+City: Hyderabad`;
+
+  window.open(
+    "https://wa.me/918790966832?text=" + encodeURIComponent(msg),
+    "_blank"
+  );
+function openWhatsApp(message) {
+  window.open(
+    "https://wa.me/918790966832?text=" + encodeURIComponent(message),
+    "_blank"
+  );
+}
+
+  form.reset();
+}
